bba9f45b3d2218d99c5cc5e678276dc1b50c52ce
dassoham030701 <soham1937@gmail.com>
2025-09-08 06:55:09 +0530

    Deployment Setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,45 @@
+// import 'dotenv/config';
+// import {app} from './app.js';
+// import connectDB from './db/db.js';
+// import path from 'path';
+//  await connectDB()
+
+// const __dirname=path.resolve();
+// app.use(express.static(path.join(__dirname,'./frontend/dist')))
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+// })
+
+// const PORT=process.env.PORT || 3000;
+
+// app.listen(PORT,()=>{
+//     console.log('Server is running on port:',PORT)
+// })
+
+
 import 'dotenv/config';
-import {app} from './app.js';
+import express from 'express';
+import { app } from './app.js';
 import connectDB from './db/db.js';
+import path from 'path';
 
- await connectDB()
+const __dirname = path.resolve();
 
-const PORT=process.env.PORT || 3000;
+// connect DB
+await connectDB();
 
-app.listen(PORT,()=>{
-    console.log('Serveer is running on port:',PORT)
-})
+// serve static frontend build
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server is running on port:', PORT);
+});
